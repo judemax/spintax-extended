d2b4ba5400b5ae0c7e0f2f1afe26cb9f4cd6f5d2
taokef <104082225+judemax@users.noreply.github.com>
2022-06-25 17:38:42 +0300

    main functional
    Wrote main functional
    Add examples

---
+++ examples/examples.js
@@ -0,0 +1,10 @@
+const Spintax=require('../');
+
+console.log(Spintax.unspin("Wow text without spintax! Amazing!"));
+console.log(Spintax.unspin("Oh, look, {Red|White|Blue} {water|star|color}!"));
+console.log(Spintax.unspin("\\[Red|[Whi|Te]{Whi\\|te}|Blue\\]"));
+console.log(Spintax.unspin("A[Red|2 [+\\++Whi|Te] {1|2}|Blue]"));
+console.log(Spintax.unspin("[Red|White|Blue]"));
+console.log(Spintax.unspin("[+_+Red|White|Blue]"));
+console.log(Spintax.unspin("[+{_|-}+Red|White|Blue {1|2}]"));
+console.log(Spintax.unspin("[+\\++Red|\\[White\\]|Blue]"));
---
+++ index.js
@@ -0,0 +1,5 @@
+/*
+ * Copyright (c) 2022 Jude Max <judemaxgthb@gmail.com>
+ */
+
+module.exports=require('./lib/spintax');
